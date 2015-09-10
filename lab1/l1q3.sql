1a381cd1a178f41050a577ed411f0ea87ea64052
Rasmus Ansin <rasmus.ansin@gmail.com>
2015-09-10 16:04:15 +0200

    Added answers for l1q3.sql

--- lab1/l1q3.sql
+++ lab1/l1q3.sql
@@ -10,4 +10,17 @@ FROM  ( SELECT last_name,first_name,book_id -- alla kunder som köpt någon bok.
              FROM books, subjects
              WHERE subject='Horror'
              AND subjects.subject_id=books.subject_id)HorrorBook
-WHERE BookBuyer.book_id=HorrorBook.book_id;
+WHERE BookBuyer.book_id=HorrorBook.book_id; last_name | first_name 
+/*
+-----------+------------
+ Brown     | Chuck
+ Gould     | Ed
+ Holloway  | Adam
+ Black     | Jean
+ King      | Jenny
+ Anderson  | Jonathan
+ Becker    | Owen
+ Morrill   | Royce
+ Jackson   | Annie
+(9 rows)
+*/
