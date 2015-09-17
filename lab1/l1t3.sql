85b0bc754d4b4848ddc9cbb5977eaa512cd15328
Sander Riedberg <riedberg@gmail.com>
2015-09-17 15:42:27 +0200

    re-saved l1t3.sql

--- lab1/l1t3.sql
+++ lab1/l1t3.sql
@@ -12,7 +12,8 @@ BEFORE INSERT ON shipments      FOR EACH ROW             EXECUTE PROCEDURE dec
 
 
 /*
-SELECT * FROM stock;--Output: the whole table before making shipments.INSERT INTO shipmentsVALUES(2000, 860, '0394900014', '2012-12-07');--Since this book is not in stock, this should give: Output: ERROR: There is no stock to shipINSERT INTO shipmentsVALUES(2001, 860, '044100590X', '2012-12-07');--This should giveOutput: INSERT 0 1--Then test by:SELECT * FROM shipments WHERE shipment_id > 1999; --Which should show that only the second shipment was actually inserted: Output: 2001 | 860 | 044100590X | 2012-12-07 00:00:00+01SELECT * FROM stock;--Output: the stock table with 044100590X decremented.--Remember to then restore the database to its original state (in the .sql file): 
+SELECT * FROM stock;--Output: the whole table before making shipments.INSERT INTO shipmentsVALUES(2000, 860, '0394900014', '2012-12-07');--Since this book is not in stock, this should give: Output: ERROR: There is no stock to shipINSERT INTO shipmentsVALUES(2001, 860, '044100590X', '2012-12-07');--This should giveOutput: INSERT 0 1--Then test by:SELECT * FROM shipments WHERE shipment_id > 1999; 
+--Which should show that only the second shipment was actually inserted: Output: 2001 | 860 | 044100590X | 2012-12-07 00:00:00+01SELECT * FROM stock;--Output: the stock table with 044100590X decremented.--Remember to then restore the database to its original state (in the .sql file): 
 DELETE FROM shipments WHERE shipment_id > 1999;--Output: DELETE 1UPDATE stock SET stock = 89 WHERE isbn = '044100590X'; 
 --Output: UPDATE 1--I would also end my sql file withDROP FUNCTION decstock() CASCADE;
 
